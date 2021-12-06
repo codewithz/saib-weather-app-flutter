b5b9a4fd4cf14e2b2f85c78fffff80785537a880
Zartab Nakhwa <zartab@codewithz.com>
2021-12-06 18:45:35 +0530

    Http Calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
 
@@ -22,6 +23,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getHttpData() async{
+    var url=Uri.parse('https://localhost:9090/accounts/100100101');
+    Response response=await get(url);
+    print(response.body);
+    print(response.statusCode);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -34,7 +42,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: (){
-
+          getHttpData();
             //Get the current location
           },
           child: Text('Get Location'),
