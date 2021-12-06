f3fb3f376c2bb2384f81f449bf8c3d8c15d81ef2
Zartab Nakhwa <zartab@codewithz.com>
2021-12-06 16:08:02 +0530

    Fonts and COnstants added

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+    const kTempTextStyle=TextStyle(
+      fontFamily: 'Spartan',
+      fontSize: 100
+    );
+
+    const kMessageTextStyle=TextStyle(
+    fontFamily: 'Spartan',
+    fontSize: 60
+    );
+
+    const kButtonTextStyle=TextStyle(
+        fontFamily: 'Spartan',
+        fontSize: 30
+    );
+
+    const kConditionTextStyle=TextStyle(
+        fontSize: 100
+    );
